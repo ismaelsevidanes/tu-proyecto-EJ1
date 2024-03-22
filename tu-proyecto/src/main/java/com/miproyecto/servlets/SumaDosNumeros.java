1e1eefa093f90d5717d16d0f3c0c28e7abf1d1ac
ismaelsevidanes <alum.isevidanesd@iesalixar.org>
2024-03-22 11:09:34 +0100

    ejercicio 2 terminado

---
+++ tu-proyecto/src/main/java/com/miproyecto/servlets/SumaDosNumeros.java
@@ -0,0 +1,47 @@
+package com.miproyecto.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class SumaDosNumeros
+ */
+@WebServlet("/suma")
+public class SumaDosNumeros extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	
+    public SumaDosNumeros() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        String numero1Str = request.getParameter("numero1");
+        String numero2Str = request.getParameter("numero2");
+
+        Integer numero1 = Integer.parseInt(numero1Str);
+        Integer numero2 = Integer.parseInt(numero2Str);
+	    
+	      response.getWriter().print(numero1 + numero2);	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
